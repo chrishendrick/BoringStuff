f05bb310795072b5c5d10083c8e5d7aae8a124b2
Chris Hendrick <chendric@mathworks.com>
2019-01-03 10:44:40 -0500

    hello

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+# This program says Hello and asks for my name
+
+print('Hello world.')
+myName = input('What is your name? ') #ask for their name
+print('It is good to meet you, ' + myName)
+print('The length of your name is: ' + str(len(myName)))
+myAge = input('What is your age? ') #ask for their age
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
